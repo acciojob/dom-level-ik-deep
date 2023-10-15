6660c93447e97b459d77dfc64f190302602fe42d
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-15 15:54:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the target element by its id
+    let element = document.getElementById("level");
+  
+    // Initialize a variable to count the DOM level
+    let domLevel = 1;
+  
+    // Traverse the parent elements until we reach the root html element
+    while (element && element !== document.documentElement) {
+      element = element.parentElement;
+      domLevel++;
+    }
+  
+    // Display the result using the alert() function
+    alert(`The level of the element is: ${domLevel}`);
+  });
